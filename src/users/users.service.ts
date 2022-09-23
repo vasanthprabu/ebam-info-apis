4c7d69e365c5ea12102bcc034d136edbeae33cdd
vasanthprabu <82998298+vasanthprabu@users.noreply.github.com>
2022-09-23 22:47:12 +0530

    Backend nest apis with auth guards

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'prabu',
+        password: 'Test@123',
+      },
+      {
+        userId: 2,
+        username: 'karthik',
+        password: 'Test@123',
+      },
+      {
+        userId: 3,
+        username: 'maria',
+        password: 'guess',
+      },
+    ];
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
